b8adefed9ba273aaa3779f9a52aa5165bf083977
Juan Rodríguez <juansero29@gmail.com>
2020-11-19 16:22:01 +0100

    - reduced code to maintain - added small hill - added Hardcore generic class

--- SFDScripts/Internal/Hardcore/HardcoreSmallHill.cs
+++ SFDScripts/Internal/Hardcore/HardcoreSmallHill.cs
@@ -5,9 +5,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SFDScripts.Internal.Hardcore
+namespace SFDScripts
 {
-    class HardcoreSmallHill : GameScriptInterface
+    class HardcoreSmallHill : Hardcore
     {
+        #region Map Dependant Data
+        /// <summary>
+        /// The number of map parts in this map. For each map part, an startup process
+        /// will be done in the 'OnStartup' method.
+        /// </summary>
+        public static int NumberOfMapParts = 1;
+
+
+        /// <summary>
+        /// Defines the number of wins required per map part to advance
+        /// </summary>
+        /// <remarks>
+        /// Has to be an uneven number to avoid ties.
+        /// </remarks>
+        public static int RoundsPerMapPart = 3;
+
+        /// <summary>
+        /// Defines the map part that we should start on
+        /// </summary>
+        public static int CurrentMapPartIndex = 0;
+
+        /// <summary>
+        /// The world top position (where airstrikes get launched from)
+        /// </summary>
+        public static int WorldTop = 250;
+
+        #region Drones
+        /// <summary>
+        /// Left bottom corner spot of the map. (x and  y coordinates)
+        /// </summary>
+        public static Vector2 DroneAreaBegin = new Vector2(-316, -244);
+        /// <summary>
+        /// An area that covers each of the map parts (x = width, y = height)
+        /// </summary>
+        public static Vector2 DroneAreaSize = new Vector2(640, 480);
+        #endregion
+
+        public static int CameraWidth = 640;
+        public static int CameraHeight = 480;
+        #endregion
     }
 }
