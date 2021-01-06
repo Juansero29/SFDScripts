816f44f26b0097733b5b3093809f6e75a022f547
Juan Rodríguez <juansero29@gmail.com>
2021-01-06 12:31:52 +0100

    Solves #17 - created hardcore classic remastered

---
+++ SFDScripts/Internal/Hardcore/HardcoreClassicRemastered.cs
@@ -0,0 +1,53 @@
+﻿using SFDGameScriptInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFDScripts
+{
+    class HardcoreClassicRemastered : Hardcore
+    {
+        #region Map Dependant Data
+        /// <summary>
+        /// The number of map parts in this map. For each map part, an startup process
+        /// will be done in the 'OnStartup' method.
+        /// </summary>
+        public static int NumberOfMapParts = 3;
+
+
+        /// <summary>
+        /// Defines the number of wins required per map part to advance
+        /// </summary>
+        /// <remarks>
+        /// Has to be an uneven number to avoid ties.
+        /// </remarks>
+        public static int RoundsPerMapPart = 1;
+
+        /// <summary>
+        /// Defines the map part that we should start on
+        /// </summary>
+        public static int CurrentMapPartIndex = 1;
+
+        /// <summary>
+        /// The world top position (where airstrikes get launched from)
+        /// </summary>
+        public static int WorldTop = 500;
+
+        #region Drones
+        /// <summary>
+        /// Left bottom corner spot of the map. (x and  y coordinates)
+        /// </summary>
+        public static Vector2 DroneAreaBegin = new Vector2(-1120, 2);
+        /// <summary>
+        /// An area that covers each of the map parts (x = width, y = height)
+        /// </summary>
+        public static Vector2 DroneAreaSize = new Vector2(3680, 904); 
+        #endregion
+
+        public static int CameraWidth = 768;
+        public static int CameraHeight = 552;
+        #endregion
+    }
+}
