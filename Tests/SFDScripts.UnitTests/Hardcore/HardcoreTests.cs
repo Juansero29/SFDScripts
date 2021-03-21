f71272b0fe8c62555bff4702d530cec1f3b27d24
Juan Rodríguez <juansero29@gmail.com>
2021-03-21 18:16:10 +0100

    Started adding unit tests

---
+++ Tests/SFDScripts.UnitTests/Hardcore/HardcoreTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SFDGameScriptInterface;
+using System;
+
+namespace SFDScripts.UnitTests
+{
+    [TestClass]
+    public class HardcoreTests
+    {
+        private Mock<IGame> GameMock;
+        private Hardcore HardcoreGameInstance;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+        }
+
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var hardcore = new Hardcore();
+            Assert.IsNotNull(hardcore);
+        }
+
+        [TestMethod]
+        public void OnStartupTest()
+        {
+            var hardcore = new Hardcore();
+            hardcore.OnStartup();
+            Assert.IsNotNull(hardcore);
+        }
+    }
+}
---
+++ Tests/SFDScripts.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SFDScripts.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SFDScripts.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59b08e91-a094-445d-a327-facd7962a996")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
