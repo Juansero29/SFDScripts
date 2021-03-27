819512d4f2a9b87092cee0dd40a0800fc041c32c
Juan Rodríguez <juansero29@gmail.com>
2021-03-27 15:55:21 +0100

    adding admin privileges

---
+++ run_as.ps1
@@ -0,0 +1,51 @@
+param
+(
+  [string]$Filepath = $(throw "File absolute path is missing"),
+  [string]$Arguments
+)
+
+# Get the ID and security principal of the current user account
+$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent();
+$myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID);
+
+# Get the security principal for the administrator role
+$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
+
+# Check to see if we are currently running as an administrator
+if($myWindowsPrincipal.IsInRole($adminRole))
+{
+    # We are running as an administrator, so change the title and background colour to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)";
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue";
+    Clear-Host;
+}
+else
+{
+    # We are not running as an administrator, so relaunch as administrator
+
+    # Create a new process object that starts PowerShell
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+    # Specify the current script path and name as a parameter with added scope and support for scripts with spaces in it's path
+    #-noexit
+    $newProcess.Arguments =  " & '" + $script:MyInvocation.MyCommand.Path + "' '" + $Filepath + "' '" + $Arguments + "'"
+
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+
+    # Exit from the current, unelevated, process
+    Exit;
+}
+
+Write-Host File path is  $Filepath;
+
+Write-Host Arguments are $Arguments;
+
+$commandline = "& `"$Filepath`" $Arguments"
+
+Write-Host Executing $commandline;
+
+Invoke-Expression  $commandline
